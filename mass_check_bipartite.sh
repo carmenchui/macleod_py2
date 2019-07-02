5de8518bddb690aeaef5704b1352d2aea5c96fd6
carmenchui <cchui@mie.utoronto.ca>
2019-07-02 11:46:20 -0400

    Create mass_check_bipartite.sh

---
+++ mass_check_bipartite.sh
@@ -0,0 +1,4 @@
+cd /Users/cchui/Documents/GitHub/macleod_py2
+python2 tasks/check_consistency.py /Users/cchui/Documents/GitHub/colore/ontologies/bipartite_incidence/affine_plane.clif
+python2 tasks/check_consistency.py /Users/cchui/Documents/GitHub/colore/ontologies/bipartite_incidence/beutel_nondeg_projective_space.clif
+python2 tasks/check_consistency.py /Users/cchui/Documents/GitHub/colore/ontologies/bipartite_incidence/unique_boundary_incidence.clif
